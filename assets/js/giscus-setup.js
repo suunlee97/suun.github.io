23af1412f57871d81f57b10e0661b977e0555dfa
suunlee97 <161161812+suunlee97@users.noreply.github.com>
2026-02-06 19:48:01 +0000

    Deploying to gh-pages from @ suunlee97/suunlee97.github.io@b493761def88bdb146ed8aeedcc7fbb1d2c97fe0 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "suunlee97/suun.github.io",
+    "data-repo": "suunlee97/suunlee97.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
